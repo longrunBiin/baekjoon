5d8a63c1ab3962f4655b061e683b01d68d5010ef
subinjang <eldnj521@gmail.com>
2023-02-14 01:06:51 +0900

    1406 clear(stack version)

--- stack/1406/main.cpp
+++ stack/1406/main.cpp
@@ -6,8 +6,8 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	stack<char> st;
-	stack<char> tmp_st;
+	stack<char> stLeft;
+	stack<char> stRight;
 	string s;
 	int M, index, length;
 	cin >> s;
@@ -15,56 +15,40 @@ int main() {
 	index = s.size();
 	length = index;
 	for (int i = 0; i < s.size(); i++)
-		st.push(s[i]);
+		stLeft.push(s[i]);
 	while (M--) {
-		int size = st.size();
 		char command;
 		char n;
 		cin >> command;
 		if (command == 'P') {
 			cin >> n;
-			for (int i = 0; i < size-index; i++) {
-				tmp_st.push(st.top());
-				st.pop();
-			}
-			st.push(n);
-			for (int i = 0; i < size - index; i++) {
-				st.push(tmp_st.top());
-				tmp_st.pop();
-			}
-			if (index < length + 1)
-				index++;
+			stLeft.push(n);
 		}
 		else if (command == 'L') {
-			if (index > 0)
-				index--;
+			if (!stLeft.empty()) {
+				stRight.push(stLeft.top());
+				stLeft.pop();
+			}
 		}
 		else if (command == 'D') {
-			if (index < length +1)
-				index++;
+			if (!stRight.empty()) {
+				stLeft.push(stRight.top());
+				stRight.pop();
+			}
 		}
 		else if (command == 'B') {
-			for (int i = 0; i < size - index; i++) {
-				tmp_st.push(st.top());
-				st.pop();
-			}
-			if (!st.empty()||index!=0)
-				st.pop();
-			for (int i = 0; i < size - index; i++) {
-				st.push(tmp_st.top());
-				tmp_st.pop();
-			}
-			if (index > 0)
-				index--;
+			if (!stLeft.empty())
+				stLeft.pop();
 		}
 	}
-	while (!st.empty()) {
-		tmp_st.push(st.top());
-		st.pop();
+	while (!stLeft.empty()) {
+		stRight.push(stLeft.top());
+		stLeft.pop();
 	}
-	while (!tmp_st.empty()) {
-		cout << tmp_st.top();
-		tmp_st.pop();
+	while (!stRight.empty()) {
+		cout << stRight.top();
+		stRight.pop();
 	}
+		
 	
 }
