57c0b0ba785484f339f0b4d7fb0534f5e0b749bd
subinjang <eldnj521@gmail.com>
2023-02-13 23:57:42 +0900

    9093 clear

--- stack/9093/main.cpp
+++ stack/9093/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <stack>
+#include <string>
 using namespace std;
 
 int main() {
-
+	int T;
+	cin >> T;
+	cin.ignore();
+	while (T--) {
+		int i = 0;
+		stack<char> stack;
+		string s;
+		getline(cin, s);
+		s += ' ';
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == ' ') {
+				while (!stack.empty()) {
+					cout << stack.top();
+					stack.pop();
+				}cout << ' ';
+			}
+			else stack.push(s[i]);
+		}
+		cout << '\n';
+	}
 }
