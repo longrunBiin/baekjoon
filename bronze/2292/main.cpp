7afaea7f94d6f8e491223b54b4bd7859bd3a144f
subinjang <eldnj521@gmail.com>
2023-03-01 21:30:57 +0900

    2292 clear

--- bronze/2292/main.cpp
+++ bronze/2292/main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+	int N, count=1,bee=1;
+	cin >> N;
 
+	while (true) {
+		if (N == 1) {
+			cout << N;
+			return 0;
+		}
+		else if (N > bee && N <= count*6 + bee) {
+			cout << count + 1;
+			return 0;
+		}
+		else {
+			bee += 6*count;
+			count++;
+		}
+	}
 }
