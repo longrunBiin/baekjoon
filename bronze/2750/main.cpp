fe600901542c7f0f9bbc48f4b09e4a741fbf37d3
subinjang <eltm521@naver.com>
2023-03-07 14:21:50 +0900

    2750 clear

--- bronze/2750/main.cpp
+++ bronze/2750/main.cpp
@@ -2,19 +2,27 @@
 using namespace std;
 
 int main() {
-	int N, num[1000]={0,},i=0;
+	int N, num[1000] = { 0, }, q = 0, tmp;
 	cin >> N;
-	while (N--) {
-		int n,i=0,tmp;
+	tmp = N;
+	while (tmp--) {
+		int n;
 		cin >> n;
-		for (int j = 0; j < i; j++) {
-			if (num[j] > n) {
-				tmp = num[j];
-				num[j] = n;
-
-
+		num[q++] = n;
+	}
+	for (int i = 0; i < N; i++) {
+		int min = num[i];
+		tmp = i;
+		for (int j = i; j < N; j++) {
+			if (min > num[j]) {
+				min = num[j];
+				tmp = j;
 			}
 		}
+		num[tmp] = num[i];
+		num[i] = min;
 	}
-
+	for (int i = 0; i < N; i++) {
+		cout << num[i] << "\n";
+	}
 }
