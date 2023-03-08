d18da107b3d888c6f00f826d531d964f9bd78a45
subinjang <eltm521@naver.com>
2023-03-08 11:02:51 +0900

    2108 fail

--- silver/2108/main.cpp
+++ silver/2108/main.cpp
@@ -4,45 +4,53 @@
 #include<cmath>
 using namespace std;
 
+
 int main() {
-	int N, sum = 0, mid = 0, fre = 0, boundary = 0;
+	int N, sum = 0, mid = 0, boundary = 0;
 	double avg;
 	vector<int> v;
 	cin >> N;
 	int freq[8001] = { 0, };
+	int n;
 	for (int i = 0; i < N; i++) {
-		int n;
 		cin >> n;
 		v.push_back(n);
 		sum += n;
-		if (n >= 0)
-			freq[n];
+		if (n < 0)
+			freq[n]++;
 		else
-			freq[(- n) + 4000]++;
+			freq[n + 4000]++;
 	}
-	avg = floor((sum / N) + 0.5);
+	if (N == 1) avg = n;
+	else if (sum >= 0)
+		avg = floor((sum / N) + 0.5);
+	else if (sum/N<0.5 && sum/N>-0.5)
+		avg = 0;
+	else
+		avg = floor((sum / N) -0.5);
 	sort(v.begin(), v.end());
 	mid = v[N/2];
-	fre = freq[0];
-	int tmp;
-	bool flag = true;
-	for (int i = 1; i < 8001; i++) {
-		if (fre < freq[i]) {
-			fre = freq[i];
-			if (i > 4000)
-				tmp = i-4000;
-			else
-				tmp = i;
+	int max=0;
+	bool flag = false;
+	for (int i = 0; i < 8001; i++) {
+		if (max < freq[i]) {
+			max = freq[i];
 		}
-		else if (fre == freq[i]) {
-			if (i > 4000)
-				tmp = i - 4000;
-			else
-				tmp = i;
-			break;
+		else if (max != 0 && max == freq[i]) flag = true;
+	}
+	flag = false;
+	if (flag) {
+		for (int i = 0; i < 8001; i++) {
+			if (flag) { 
+				if (i > 4000) max = i - 4000;
+				else
+					max = -i;
+				break;
+			}
+			if (max == freq[i]) flag == true;
 		}
 	}
-	
+	if (N == 1) max = n;
 	boundary = v.back() - v.front();
-	cout << avg << "\n" << mid << "\n" << tmp << "\n" << boundary;
+	cout << avg << "\n" << mid << "\n" << max << "\n" << boundary;
 }
