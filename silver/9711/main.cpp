75ad817a32bacba5ca9931b15a67c5e2c01bca44
subinjang <eldnj521@gmail.com>
2023-03-19 22:04:42 +0900

    9711 clear

--- silver/9711/main.cpp
+++ silver/9711/main.cpp
@@ -1,22 +1,25 @@
 #include<iostream>
 using namespace std;
 
+long long fibo(int p, int q) {
+	long long n1 = 0;
+	long long n2 = 1;
+	for (int i = 0; i < p; i++) {
+		long long n3 = n1 + n2;
+		if (n3 > q) n3 %= q;
+		n1 = n2;
+		n2 = n3;
+	}
+	return n1;
+}
+
 int main() {
 	int P, Q, T, i = 1;
 	cin >> T;
 	while (T--) {
 		long long f1 = 0, f2 = 1, f3, result, tmp;
 		cin >> P >> Q;
-		
-		for (int i = 3; i <= P; i++) {
-			f3 = f1 + f2;
-			if (f3 > Q) f3 %= Q;
-			f1 = f2;
-			f2 = f3;
-
-		}
-
-		result = f1%Q;
+		result = fibo(P, Q) % Q;
 		cout << "Case #" << i++ << ": " << result << "\n";
 	}
 }
