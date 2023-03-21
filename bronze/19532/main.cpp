487bf1139778293d5d4b256c22d60957b93bc367
subinjang <eldnj521@gmail.com>
2023-03-21 21:53:30 +0900

    .

---
+++ bronze/19532/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, d, e, f, x, y;
+	cin >> a >> b >> c >> d >> e >> f;
+
+	for (int i = -999; i <= 999; i++) {
+		for(int j = -999; j <= 999; j++)
+		i = (c - a * j) / b;
+	}
+	y = (f - d * x) / e;
+	
+}
