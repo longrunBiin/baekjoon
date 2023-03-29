11fa7c0fedbe464ccab0a8abaebc151e0270fc56
subinjang <eldnj521@gmail.com>
2023-03-29 22:52:30 +0900

    1620 clear

--- silver/1620/main.cpp
+++ silver/1620/main.cpp
@@ -1,33 +1,29 @@
 #include<iostream>
 #include<vector>
+#include<map>
 #include<string>
 using namespace std;
 int main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(NULL);
 	int M, N, i = 0;
 	cin >> N >> M;
-
-	vector<pair<int, string>> v;
-	while (N--) {
-		string n;
-		cin >> n;
-		v.push_back(make_pair(i, n));
-		i++;
+	map<string, int> pokemon;
+	vector<string> name;
+	string tmp;
+	
+	for (int i = 1; i <= N; i++) {
+		cin >> tmp;
+		name.push_back(tmp);
+		pokemon.insert(make_pair(tmp, i));
 	}
-	for (int q = 0; q < M; q++) {
-		string ans;
-		cin >> ans;
-		
-		if (ans[0] >= 65 && ans[0] <= 90){
-			for (int k = 0; k < v.size(); k++) {
-				if (v[k].second == ans) {
-					cout << k+1 << "\n";
-					break;
-				}
-			}
+	for (int i = 1; i <= M; i++) {;
+		cin >> tmp;
+		if (tmp[0] >= 65 && tmp[0] <= 90) {
+			cout << pokemon[tmp] << "\n";
 		}
 		else {
-			int num = stoi(ans);
-			cout << v[num-1].second << "\n";
+			cout << name[stoi(tmp)-1] << "\n";
 		}
 	}
 }
