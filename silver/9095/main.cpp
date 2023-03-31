55b379e6c5efbd79ab32a0b20ddfc51120c52af4
subinjang <eldnj521@gmail.com>
2023-03-31 23:00:54 +0900

    9095 clear

--- silver/9095/main.cpp
+++ silver/9095/main.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 using namespace std;
 int main() {
-	int dp[11];
+	int dp[11] = {0,};
 	int N;
 	cin >> N;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	for (int i = 4; i <= 10; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
 	while (N--) {
 		int n;
 		cin >> n;
-		dp[1] = 0;
-		dp[2] = 1;
-		dp[3] = 3;
-		for (int i = 4; i <= n; i++) {
-
-		}
+		cout << dp[n] << "\n";
 	}
 }
